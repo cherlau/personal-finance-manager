6b07ae0c5024ea8f2c806f8e487f9378378c7f96
Cherlau <cherlau_prado@hotmail.com>
2024-11-15 16:02:54 -0300

    adicionado o prisma-binding e configurado

--- vue-finances-back/src/index.js
+++ vue-finances-back/src/index.js
@@ -1,21 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga')
+const Binging = require('prisma-binding')
 const { prisma } = require('./generated/prisma-client')
 
-const resolvers = {
-	Query: {
-		user(parent, args, context, info) {
-			return prisma.user({ id: args.id })
-				.then(user => {
-					console.log('user: ', user)
-					return user
-				})
-		}
-	}
-}
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
 	typeDefs: `${__dirname}/schema.graphql`,
-	resolvers
+	resolvers,
+	context: {
+		db: new Binging.Prisma({
+			typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+			endpoint: process.env.PRISMA_ENDPOINT,
+		}),
+		prisma
+	}
 })
 
 server.start().then(() => console.log('Server running on http://localhost:4000...'))
