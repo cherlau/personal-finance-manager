30360f108a70e57fffcd82d2098b45faf64c6db7
Cherlau <cherlau_prado@hotmail.com>
2024-11-15 14:49:38 -0300

    Criado os dockers e configurados para rodar corretamente

---
+++ vue-finances-back/src/index.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+
+const resolvers = {
+	Query: {
+		user(parent, args, context, info) {
+			return prisma.user({ id: args.id })
+				.then(user => {
+					console.log('user: ', user)
+					return user
+				})
+		}
+	}
+}
+
+const server = new GraphQLServer({
+	typeDefs: `${__dirname}/schema.graphql`,
+	resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000...'))
