8d518b21177bfdf9726b0857eb81e62d372c557a
Cherlau <cherlau_prado@hotmail.com>
2024-11-17 15:45:35 -0300

    implementado validação do formulário de login

--- vue-finances-front/src/modules/auth/views/Login.vue
+++ vue-finances-front/src/modules/auth/views/Login.vue
@@ -6,32 +6,58 @@
         <v-card class="elevation-12">
 
           <v-toolbar color="primary" dark>
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>{{ texts.toolbar }}</v-toolbar-title>
           </v-toolbar>
 
           <v-card-text>
             <v-form>
               <v-text-field
+                v-if="!isLogin"
+                prepend-icon="person"
+                name="name"
+                label="Nome"
+                type="text"
+                :error-messages="nameErrors"
+                :success="!$v.user.name.$invalid"
+                v-model.trim="$v.user.name.$model"
+                ></v-text-field>
+              <v-text-field
                 prepend-icon="email"
                 name="email"
                 label="Email"
                 type="email"
+                :error-messages="emailErrors"
+                :success="!$v.user.email.$invalid"
+                v-model.trim="$v.user.email.$model"
                 ></v-text-field>
               <v-text-field
                 prepend-icon="lock"
                 name="password"
                 label="Senha"
                 type="password"
+                :error-messages="passwordErrors"
+                :success="!$v.user.password.$invalid"
+                v-model.trim="$v.user.password.$model"
                 ></v-text-field>
             </v-form>
-            <v-btn block depressed color="secondary">
-              Criar conta
+            <v-btn
+              block
+              depressed
+              color="secondary"
+              @click="isLogin = !isLogin"
+            >
+              {{ texts.button}}
             </v-btn>
           </v-card-text>
 
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" large>Login</v-btn>
+            <v-btn
+              :disabled="$v.$invalid"
+              color="primary"
+              large
+              @click="submit"
+            >{{ texts.toolbar }}</v-btn>
           </v-card-actions>
 
         </v-card>
@@ -42,7 +68,85 @@
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      isLogin: true,
+      user: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  validations () {
+    const validations = {
+      user: {
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        }
+      }
+    }
+
+    if (this.isLogin) { return validations }
+
+    return {
+      user: {
+        ...validations.user,
+        name: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+  },
+  computed: {
+    texts () {
+      return this.isLogin
+        ? { toolbar: 'Entrar', button: 'Criar conta' }
+        : { toolbar: 'Criar Conta', button: 'Já tenho uma conta' }
+    },
+    nameErrors () {
+      const errors = []
+      const name = this.$v.user.name
+      if (!name.$dirty) return errors
+      !name.required && errors.push('O nome é obrigatória')
+      !name.minLength && errors.push(`O nome deve ter no mínimo ${name.$params.minLength.min} caracteres`)
+      return errors
+    },
+    emailErrors () {
+      const errors = []
+      const email = this.$v.user.email
+      if (!email.$dirty) return errors
+      !email.required && errors.push('O email é obrigatório')
+      !email.email && errors.push('O email deve ser válido')
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      const password = this.$v.user.password
+      if (!password.$dirty) return errors
+      !password.required && errors.push('A senha é obrigatória')
+      !password.minLength && errors.push(`A senha deve ter no mínimo ${password.$params.minLength.min} caracteres`)
+      return errors
+    }
+  },
+  methods: {
+    log () {
+      console.log('teste')
+      console.log('Vuelidate', this.$v)
+    },
+    submit () {
+      console.log('User', this.user)
+    }
+  }
 }
 </script>
