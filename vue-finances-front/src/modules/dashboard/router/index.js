304698bb1274457a97440ee43617a6f2bcb592ba
Cherlau <cherlau_prado@hotmail.com>
2024-11-18 00:06:51 -0300

    redirecionamento para rota de login de usuário não autenticados, adicionado middleware para acrescentar token JWT nas requisições

--- vue-finances-front/src/modules/dashboard/router/index.js
+++ vue-finances-front/src/modules/dashboard/router/index.js
@@ -3,6 +3,9 @@ const Dashboard = () => import('../views/Dashboard.vue')
 export default [
   {
     path: '/dashboard',
-    component: Dashboard
+    component: Dashboard,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
--- vue-finances-front/src/router/index.js
+++ vue-finances-front/src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import authRoutes from '@/modules/auth/router'
-import dashboardRoutes from '@/modules/dashboard/router'
+import { AUTH_TOKEN } from '@/plugins/apollo'
+import AuthService from '@/modules/auth/services/auth-service'
+import AuthRoutes from '@/modules/auth/router'
+import DashboardRoutes from '@/modules/dashboard/router'
 
 Vue.use(VueRouter)
 
 const routes = [
-  ...authRoutes,
-  ...dashboardRoutes,
+  ...AuthRoutes,
+  ...DashboardRoutes,
   { path: '', redirect: '/login' }
 ]
 
@@ -16,6 +18,31 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  console.log(to)
+
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const token = window.localStorage.getItem(AUTH_TOKEN)
+    const loginRoute = {
+      path: '/login',
+      query: { redirect: to.fullPath }
+    }
+
+    if (token) {
+      try {
+        console.log('Fetching user...')
+        await AuthService.user({ fetchPolicy: 'network-only' })
+        return next()
+      } catch (error) {
+        console.error(error)
+        return next(loginRoute)
+      }
+    }
+    return next(loginRoute)
+  }
+  next()
 })
 
 export default router
